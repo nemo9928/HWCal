1e78e0bde29b610c86d45de6a231e3d10e2ef0d9
nemo9928 <154311345+nemo9928@users.noreply.github.com>
2024-11-08 18:38:42 +0900

    Update RegisterDAO.java
    change to preparedStatement

--- src/main/java/dao/RegisterDAO.java
+++ src/main/java/dao/RegisterDAO.java
@@ -3,26 +3,44 @@ package dao;
 import java.sql.Statement;
 
 public class RegisterDAO extends ConnectionDAO{
-		
+
 	public int insert(String enterprise, double mWage, double holiday, double bonus) {
-		String sql = "INSERT INTO enterprise VALUES('" + enterprise + "', '" + mWage + "','" +
-				holiday + "','" + bonus + "')";
-		Statement stmt = null;
 		int result = 0;
-		try {
-			connect();
-			stmt = con.createStatement();
-			result = stmt.executeUpdate(sql);
+		String sql = "INSERT INTO enterprise VALUES (?, ?, ?, ?)";
+		connect();
+		try(PreparedStatement stmt = con.prepareStatement(sql)){
+			stmt.setString(1, enterprise);
+			stmt.setDouble(2, mWage);
+			stmt.setDouble(3, holiday);
+			stmt.setDouble(4, bonus);
+			result = stmt.executeUpdate();
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
-			try {
-				if(stmt != null) stmt.close();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
+			disconnect();
 		}
-		disconnect();
 		return result;
 	}
+	
+	// public int insert(String enterprise, double mWage, double holiday, double bonus) {
+	// 	String sql = "INSERT INTO enterprise VALUES('" + enterprise + "', '" + mWage + "','" +
+	// 			holiday + "','" + bonus + "')";
+	// 	Statement stmt = null;
+	// 	int result = 0;
+	// 	try {
+	// 		connect();
+	// 		stmt = con.createStatement();
+	// 		result = stmt.executeUpdate(sql);
+	// 	}catch(Exception e) {
+	// 		e.printStackTrace();
+	// 	}finally {
+	// 		try {
+	// 			if(stmt != null) stmt.close();
+	// 		}catch(Exception e) {
+	// 			e.printStackTrace();
+	// 		}
+	// 	}
+	// 	disconnect();
+	// 	return result;
+	// }
 }
