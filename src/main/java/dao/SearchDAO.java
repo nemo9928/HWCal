fb27f1b945183cd2677cdcd06c72a31db04eca3e
nemo9928 <154311345+nemo9928@users.noreply.github.com>
2024-11-08 18:39:53 +0900

    Update SearchDAO.java
    change to try-with-resourses

--- src/main/java/dao/SearchDAO.java
+++ src/main/java/dao/SearchDAO.java
@@ -9,19 +9,15 @@ import bean.EPListDTO;
 public class SearchDAO extends ConnectionDAO{
 	public EPListDTO select(String name) {
 		EPListDTO dto = new EPListDTO();
-		String sql = "";
-		if(name.equals("企業検索")) {
+		String sql = null;
+		if(name.equals("企業検索") || name.isEmpty()) {
 			sql = "select * from enterprise";
 		}else {
 			sql = "select * from enterprise where name like '" + name + "'";
 		}
-		Statement stmt = null;
-		ResultSet rs = null;
 		
-		try {
-			connect();
-			stmt = con.createStatement();
-			rs = stmt.executeQuery(sql);
+		connect();
+		try(Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(sql)){
 			while(rs.next()) {
 				EPBean bean = new EPBean();
 				bean.setName(rs.getString("name"));
@@ -33,14 +29,45 @@ public class SearchDAO extends ConnectionDAO{
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
-			try {
-				if(rs != null) rs.close();
-				if(stmt != null) stmt.close();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
+			disconnect();
 		}
-		disconnect();
 		return dto;
 	}
+	
+	// public EPListDTO select(String name) {
+	// 	EPListDTO dto = new EPListDTO();
+	// 	String sql = "";
+	// 	if(name.equals("企業検索")) {
+	// 		sql = "select * from enterprise";
+	// 	}else {
+	// 		sql = "select * from enterprise where name like '" + name + "'";
+	// 	}
+	// 	Statement stmt = null;
+	// 	ResultSet rs = null;
+		
+	// 	try {
+	// 		connect();
+	// 		stmt = con.createStatement();
+	// 		rs = stmt.executeQuery(sql);
+	// 		while(rs.next()) {
+	// 			EPBean bean = new EPBean();
+	// 			bean.setName(rs.getString("name"));
+	// 			bean.setmWage(rs.getDouble("monthWage"));
+	// 			bean.setHoliday(rs.getDouble("holiday"));
+	// 			bean.setBonus(rs.getDouble("bonus"));
+	// 			dto.add(bean);
+	// 		}
+	// 	}catch(Exception e) {
+	// 		e.printStackTrace();
+	// 	}finally {
+	// 		try {
+	// 			if(rs != null) rs.close();
+	// 			if(stmt != null) stmt.close();
+	// 		}catch(Exception e) {
+	// 			e.printStackTrace();
+	// 		}
+	// 	}
+	// 	disconnect();
+	// 	return dto;
+	// }
 }
