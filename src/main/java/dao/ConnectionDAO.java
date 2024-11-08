1e830e9977a0c44899ab6a88a239fb74a6efd91f
nemo9928 <154311345+nemo9928@users.noreply.github.com>
2024-11-08 18:46:19 +0900

    Update ConnectionDAO.java
    class -> abstract
    method -> packagePrivate

--- src/main/java/dao/ConnectionDAO.java
+++ src/main/java/dao/ConnectionDAO.java
@@ -3,13 +3,13 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class ConnectionDAO {
+public abstract class ConnectionDAO {
 	private String URL = "jdbc:mysql://localhost/enterprisesdb";
 	private String USER = "root";
 	private String PASS = "pass";
 	Connection con = null;
 	
-	public void connect() {
+	void connect() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection(URL, USER, PASS);
@@ -19,7 +19,7 @@ public class ConnectionDAO {
 	}
 	
 	
-	public void disconnect() {
+	void disconnect() {
 		try {
 			if(con != null)con.close();
 		}catch(Exception e) {
